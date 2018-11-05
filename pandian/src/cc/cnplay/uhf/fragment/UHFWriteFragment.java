3d18edfbc8ef03bd3d13d53d972cc7f32dcbfece
peixere <peixere@qq.com>
2018-11-06 03:36:51 +0800

    p Signed-off-by: peixere <peixere@qq.com>

--- pandian/src/cc/cnplay/uhf/fragment/UHFWriteFragment.java
+++ pandian/src/cc/cnplay/uhf/fragment/UHFWriteFragment.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.json.JSONObject;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -56,48 +55,33 @@ public class UHFWriteFragment extends KeyDwonFragment {
 			if (uiiStr != null) {
 				EtTagUii_Write.setText(uiiStr);
 				UIHelper.ToastMessage(mContext, R.string.rfid_msg_read_succ);
-				TagTask tagTask = new TagTask(uiiStr);
-				tagTask.execute((Void) null);
+				post(uiiStr);
 			} else {
 				EtTagUii_Write.setText("");
 				UIHelper.ToastMessage(mContext, R.string.uhf_msg_read_tag_fail);
 			}
 		}
 
+		public void post(final String uiiStr) {
+
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						JSONObject json = new JSONObject();
+						json.put("rfid", uiiStr);
+						Map<String, String> header = new HashMap<String, String>();
+						header.put("token", App.login.getAcctoken());
+						String url = App.url("/home/store/tag");
+						HttpUtils.postJSON(url, json, header);
+					} catch (Exception ex) {
+						UIHelper.ToastMessage(mContext,
+								"程序异常：" + ex.toString(), 100000);
+					}
+				}
+			}).start();
+
+		}
 	}
 
-	public class TagTask extends AsyncTask<Void, Void, Boolean> {
-
-		private String rfid;
-
-		TagTask(String rfid) {
-			super();
-			this.rfid = rfid;
-		}
-
-		@Override
-		protected Boolean doInBackground(Void... params) {
-			try {
-				JSONObject json = new JSONObject();
-				json.put("rfid", rfid);
-				Map<String, String> header = new HashMap<String, String>();
-				header.put("token", App.login.getAcctoken());
-				String url = App.url("/home/store/tag");
-				String ret = HttpUtils.postJSON(url, json, header);
-				UIHelper.ToastMessage(mContext, ret);
-			} catch (Throwable ex) {
-				UIHelper.ToastMessage(mContext, ex.getMessage(), 60000);
-			}
-			return false;
-		}
-
-		@Override
-		protected void onPostExecute(final Boolean success) {
-
-		}
-
-		@Override
-		protected void onCancelled() {
-		}
-	}
 }
