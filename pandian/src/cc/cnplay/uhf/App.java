830640750b9054f252c5fba396f17d814e33e9f1
peixere <peixere@qq.com>
2018-11-05 21:49:37 +0800

    app Signed-off-by: peixere <peixere@qq.com>

---
+++ pandian/src/cc/cnplay/uhf/App.java
@@ -0,0 +1,9 @@
+package cc.cnplay.uhf;
+
+public class App {
+	public static Login login;
+	public static String PWD = "00000000";
+	public static int PORT = 0;
+	public static int CNTLEN = 0;
+	
+}
